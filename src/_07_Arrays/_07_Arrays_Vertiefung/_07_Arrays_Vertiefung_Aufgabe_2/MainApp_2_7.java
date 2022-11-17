d06295d3b50a2961e2224cfb33bef3277d434055
felmar211006 <kajdkjad@sdsdsdf>
2022-11-17 09:00:02 +0100

    Aufgaben weiter geführt

--- src/_07_Arrays/_07_Arrays_Vertiefung/_07_Arrays_Vertiefung_Aufgabe_2/MainApp_2_7.java
+++ src/_07_Arrays/_07_Arrays_Vertiefung/_07_Arrays_Vertiefung_Aufgabe_2/MainApp_2_7.java
@@ -3,20 +3,17 @@ package _07_Arrays._07_Arrays_Vertiefung._07_Arrays_Vertiefung_Aufgabe_2;
 import java.util.Arrays;
 import java.util.Random;
 
-public class MainApp_2_6 {
+public class MainApp_2_7 {
     public static void main(String[] args) {
         Random random = new Random();
 
-        int[] arr = new int[10];
-
+        int[] arr = new int[100];
         int i = 0;
 
         while (i < arr.length) {
-            arr[i] = random.nextInt(-50, 50);
+            arr[i] = random.nextInt(-50,50);
             i++;
         }
-
         System.out.println(Arrays.toString(arr));
-
     }
 }

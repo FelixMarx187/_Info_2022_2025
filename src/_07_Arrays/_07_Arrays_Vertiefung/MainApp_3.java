0e3233377acb8c6871bf529513524f47734c26cd
fridu <test@g>
2022-12-12 19:58:26 +0100

    _Info_2022_2025

--- src/_07_Arrays/_07_Arrays_Vertiefung/MainApp_3.java
+++ src/_07_Arrays/_07_Arrays_Vertiefung/MainApp_3.java
@@ -6,21 +6,33 @@ import java.util.Scanner;
 public class MainApp_3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("MAXIMUM: ");
-        int max = scanner.nextInt() - 1;
-        int[] arr = new int[max];
+        System.out.println("Maximum");
+        int max = scanner.nextInt();
 
-        int i = 0;
-        while (i < max) {
-            arr[i] = i + 2;
+        boolean[] primzahlen = new boolean[max];
+
+        int i = 2;
+        while (i < primzahlen.length) {
+            primzahlen[i] = true;
             i++;
         }
-        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(primzahlen));
 
-        //----------------------------------
+        while (i < primzahlen.length) {
+            if (primzahlen[i]) {
+                int j = 2;
+                while (i * j < primzahlen.length) {
+                    primzahlen[i * j] = false;
+                    j++;
+                }
+            }
+        }
 
-
-
+        while (i < primzahlen.length) {
+            if (primzahlen[i]) {
+                System.out.println(i);
+            }
+            i++;
+        }
     }
 }
-

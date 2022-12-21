6135c98fdbe5125aaab19ece7ff455cb23c52152
felmar <FelMar211006@gymnasiumhueck.onmicrosoft.com>
2022-12-21 15:07:25 +0100

    tET

---
+++ src/_08_Mehrdimensionale_Arrays/_08_Vertiefung/MainApp_2.java
@@ -0,0 +1,11 @@
+package _08_Mehrdimensionale_Arrays._08_Vertiefung;
+
+import java.util.Scanner;
+
+public class MainApp_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Wetterdaten 2021");
+    }
+}

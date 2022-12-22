e06b976faa18df5288a8b5818061d10e21745395
felmar211006 <kajdkjad@sdsdsdf>
2022-12-22 09:02:17 +0100

    Aufgaben weiter geführt

--- src/_08_Mehrdimensionale_Arrays/_08_Vertiefung/MainApp_A2.java
+++ src/_08_Mehrdimensionale_Arrays/_08_Vertiefung/MainApp_A2.java
@@ -1,11 +1,11 @@
 package _08_Mehrdimensionale_Arrays._08_Vertiefung;
 
+import java.util.Locale;
 import java.util.Scanner;
 
-public class MainApp_2 {
+public class MainApp_A2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Wetterdaten 2021");
     }
 }

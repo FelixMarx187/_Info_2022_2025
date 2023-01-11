70e421f3ff4f331ecce352ffbb8e172c967781d4
fridu <fridulin21@gmail.com>
2023-01-11 21:21:35 +0100

    Aufgaben weiter geführt

--- src/_08_Mehrdimensionale_Arrays/_08_Vertiefung/MainApp_A2.java
+++ src/_08_Mehrdimensionale_Arrays/_08_Vertiefung/MainApp_A2.java
@@ -28,184 +28,20 @@ public class MainApp_A2 {
         System.out.println("Monat:");
         int monat = scanner.nextInt();
 
-        if (monat == 1) {
+
+        if (monat == monat) {
             System.out.println("[0] Mittlere Temp. [C°] | [1] Minimum Temp. [C°] | [2] Maximum Temp. [C°] | [3] Niederschlag [l/m²] | [4] Regenreichster Tag [l/m²]");
-            int januar = scanner.nextInt();
-            if (januar == 0) {
-                System.out.println("Mittlere Temp. [C°]: " + wetter[0][0]);
-            } else if (januar == 1) {
-                System.out.println("Minimum Temp. [C°]. " + wetter[0][1]);
-            } else if (januar == 2) {
-                System.out.println("Maximum Temp. [C°]: " + wetter[0][2]);
-            } else if (januar == 3) {
-                System.out.println("Niederschlag [l/m²]: " + wetter[0][3]);
-            } else if (januar == 4) {
-                System.out.println("Regenreichster Tag [l/m²]: " +wetter[0][4]);
-            }
-        }
-        if (monat == 2) {
-            System.out.println("[0] Mittlere Temp. [C°] | [1] Minimum Temp. [C°] | [2] Maximum Temp. [C°] | [3] Niederschlag [l/m²] | [4] Regenreichster Tag [l/m²]");
-            int januar = scanner.nextInt();
-            if (januar == 0) {
-                System.out.println("Mittlere Temp. [C°]: " + wetter[1][0]);
-            } else if (januar == 1) {
-                System.out.println("Minimum Temp. [C°]. " + wetter[1][1]);
-            } else if (januar == 2) {
-                System.out.println("Maximum Temp. [C°]: " + wetter[1][2]);
-            } else if (januar == 3) {
-                System.out.println("Niederschlag [l/m²]: " + wetter[1][3]);
-            } else if (januar == 4) {
-                System.out.println("Regenreichster Tag [l/m²]: " +wetter[1][4]);
-            }
-        }
-        if (monat == 3) {
-            System.out.println("[0] Mittlere Temp. [C°] | [1] Minimum Temp. [C°] | [2] Maximum Temp. [C°] | [3] Niederschlag [l/m²] | [4] Regenreichster Tag [l/m²]");
-            int januar = scanner.nextInt();
-            if (januar == 0) {
-                System.out.println("Mittlere Temp. [C°]: " + wetter[2][0]);
-            } else if (januar == 1) {
-                System.out.println("Minimum Temp. [C°]. " + wetter[2][1]);
-            } else if (januar == 2) {
-                System.out.println("Maximum Temp. [C°]: " + wetter[2][2]);
-            } else if (januar == 3) {
-                System.out.println("Niederschlag [l/m²]: " + wetter[2][3]);
-            } else if (januar == 4) {
-                System.out.println("Regenreichster Tag [l/m²]: " +wetter[2][4]);
-            }
-        }
-        if (monat == 4) {
-            System.out.println("[0] Mittlere Temp. [C°] | [1] Minimum Temp. [C°] | [2] Maximum Temp. [C°] | [3] Niederschlag [l/m²] | [4] Regenreichster Tag [l/m²]");
-            int januar = scanner.nextInt();
-            if (januar == 0) {
-                System.out.println("Mittlere Temp. [C°]: " + wetter[3][0]);
-            } else if (januar == 1) {
-                System.out.println("Minimum Temp. [C°]. " + wetter[3][1]);
-            } else if (januar == 2) {
-                System.out.println("Maximum Temp. [C°]: " + wetter[3][2]);
-            } else if (januar == 3) {
-                System.out.println("Niederschlag [l/m²]: " + wetter[3][3]);
-            } else if (januar == 4) {
-                System.out.println("Regenreichster Tag [l/m²]: " +wetter[3][4]);
-            }
-        }
-        if (monat == 5) {
-            System.out.println("[0] Mittlere Temp. [C°] | [1] Minimum Temp. [C°] | [2] Maximum Temp. [C°] | [3] Niederschlag [l/m²] | [4] Regenreichster Tag [l/m²]");
-            int januar = scanner.nextInt();
-            if (januar == 0) {
-                System.out.println("Mittlere Temp. [C°]: " + wetter[4][0]);
-            } else if (januar == 1) {
-                System.out.println("Minimum Temp. [C°]. " + wetter[4][1]);
-            } else if (januar == 2) {
-                System.out.println("Maximum Temp. [C°]: " + wetter[4][2]);
-            } else if (januar == 3) {
-                System.out.println("Niederschlag [l/m²]: " + wetter[4][3]);
-            } else if (januar == 4) {
-                System.out.println("Regenreichster Tag [l/m²]: " +wetter[4][4]);
-            }
-        }
-        if (monat == 6) {
-            System.out.println("[0] Mittlere Temp. [C°] | [1] Minimum Temp. [C°] | [2] Maximum Temp. [C°] | [3] Niederschlag [l/m²] | [4] Regenreichster Tag [l/m²]");
-            int januar = scanner.nextInt();
-            if (januar == 0) {
-                System.out.println("Mittlere Temp. [C°]: " + wetter[5][0]);
-            } else if (januar == 1) {
-                System.out.println("Minimum Temp. [C°]. " + wetter[5][1]);
-            } else if (januar == 2) {
-                System.out.println("Maximum Temp. [C°]: " + wetter[5][2]);
-            } else if (januar == 3) {
-                System.out.println("Niederschlag [l/m²]: " + wetter[5][3]);
-            } else if (januar == 4) {
-                System.out.println("Regenreichster Tag [l/m²]: " +wetter[5][4]);
-            }
-        }
-        if (monat == 7) {
-            System.out.println("[0] Mittlere Temp. [C°] | [1] Minimum Temp. [C°] | [2] Maximum Temp. [C°] | [3] Niederschlag [l/m²] | [4] Regenreichster Tag [l/m²]");
-            int januar = scanner.nextInt();
-            if (januar == 0) {
-                System.out.println("Mittlere Temp. [C°]: " + wetter[6][0]);
-            } else if (januar == 1) {
-                System.out.println("Minimum Temp. [C°]. " + wetter[6][1]);
-            } else if (januar == 2) {
-                System.out.println("Maximum Temp. [C°]: " + wetter[6][2]);
-            } else if (januar == 3) {
-                System.out.println("Niederschlag [l/m²]: " + wetter[6][3]);
-            } else if (januar == 4) {
-                System.out.println("Regenreichster Tag [l/m²]: " +wetter[6][4]);
-            }
-        }
-        if (monat == 8) {
-            System.out.println("[0] Mittlere Temp. [C°] | [1] Minimum Temp. [C°] | [2] Maximum Temp. [C°] | [3] Niederschlag [l/m²] | [4] Regenreichster Tag [l/m²]");
-            int januar = scanner.nextInt();
-            if (januar == 0) {
-                System.out.println("Mittlere Temp. [C°]: " + wetter[7][0]);
-            } else if (januar == 1) {
-                System.out.println("Minimum Temp. [C°]. " + wetter[7][1]);
-            } else if (januar == 2) {
-                System.out.println("Maximum Temp. [C°]: " + wetter[7][2]);
-            } else if (januar == 3) {
-                System.out.println("Niederschlag [l/m²]: " + wetter[7][3]);
-            } else if (januar == 4) {
-                System.out.println("Regenreichster Tag [l/m²]: " +wetter[7][4]);
-            }
-        }
-        if (monat == 9) {
-            System.out.println("[0] Mittlere Temp. [C°] | [1] Minimum Temp. [C°] | [2] Maximum Temp. [C°] | [3] Niederschlag [l/m²] | [4] Regenreichster Tag [l/m²]");
-            int januar = scanner.nextInt();
-            if (januar == 0) {
-                System.out.println("Mittlere Temp. [C°]: " + wetter[8][0]);
-            } else if (januar == 1) {
-                System.out.println("Minimum Temp. [C°]. " + wetter[8][1]);
-            } else if (januar == 2) {
-                System.out.println("Maximum Temp. [C°]: " + wetter[8][2]);
-            } else if (januar == 3) {
-                System.out.println("Niederschlag [l/m²]: " + wetter[8][3]);
-            } else if (januar == 4) {
-                System.out.println("Regenreichster Tag [l/m²]: " +wetter[8][4]);
-            }
-        }
-        if (monat == 10) {
-            System.out.println("[0] Mittlere Temp. [C°] | [1] Minimum Temp. [C°] | [2] Maximum Temp. [C°] | [3] Niederschlag [l/m²] | [4] Regenreichster Tag [l/m²]");
-            int januar = scanner.nextInt();
-            if (januar == 0) {
-                System.out.println("Mittlere Temp. [C°]: " + wetter[9][0]);
-            } else if (januar == 1) {
-                System.out.println("Minimum Temp. [C°]. " + wetter[9][1]);
-            } else if (januar == 2) {
-                System.out.println("Maximum Temp. [C°]: " + wetter[9][2]);
-            } else if (januar == 3) {
-                System.out.println("Niederschlag [l/m²]: " + wetter[9][3]);
-            } else if (januar == 4) {
-                System.out.println("Regenreichster Tag [l/m²]: " +wetter[9][4]);
-            }
-        }
-        if (monat == 11) {
-            System.out.println("[0] Mittlere Temp. [C°] | [1] Minimum Temp. [C°] | [2] Maximum Temp. [C°] | [3] Niederschlag [l/m²] | [4] Regenreichster Tag [l/m²]");
-            int januar = scanner.nextInt();
-            if (januar == 0) {
-                System.out.println("Mittlere Temp. [C°]: " + wetter[10][0]);
-            } else if (januar == 1) {
-                System.out.println("Minimum Temp. [C°]. " + wetter[10][1]);
-            } else if (januar == 2) {
-                System.out.println("Maximum Temp. [C°]: " + wetter[10][2]);
-            } else if (januar == 3) {
-                System.out.println("Niederschlag [l/m²]: " + wetter[10][3]);
-            } else if (januar == 4) {
-                System.out.println("Regenreichster Tag [l/m²]: " +wetter[10][4]);
-            }
-        }
-        if (monat == 12) {
-            System.out.println("[0] Mittlere Temp. [C°] | [1] Minimum Temp. [C°] | [2] Maximum Temp. [C°] | [3] Niederschlag [l/m²] | [4] Regenreichster Tag [l/m²]");
-            int januar = scanner.nextInt();
-            if (januar == 0) {
-                System.out.println("Mittlere Temp. [C°]: " + wetter[11][0]);
-            } else if (januar == 1) {
-                System.out.println("Minimum Temp. [C°]. " + wetter[11][1]);
-            } else if (januar == 2) {
-                System.out.println("Maximum Temp. [C°]: " + wetter[11][2]);
-            } else if (januar == 3) {
-                System.out.println("Niederschlag [l/m²]: " + wetter[11][3]);
-            } else if (januar == 4) {
-                System.out.println("Regenreichster Tag [l/m²]: " +wetter[11][4]);
+            int parameter = scanner.nextInt();
+            if (parameter == 0) {
+                System.out.println("Mittlere Temp. [C°]: " + wetter[monat - 1][0]);
+            } else if (parameter == 1) {
+                System.out.println("Minimum Temp. [C°]. " + wetter[monat - 1][1]);
+            } else if (parameter == 2) {
+                System.out.println("Maximum Temp. [C°]: " + wetter[monat - 1][2]);
+            } else if (parameter == 3) {
+                System.out.println("Niederschlag [l/m²]: " + wetter[monat - 1][3]);
+            } else if (parameter == 4) {
+                System.out.println("Regenreichster Tag [l/m²]: " +wetter[monat -1][4]);
             }
         }
     }

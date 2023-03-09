66e691b08a0dc7c39a6e22ac8747a9d5a4c510da
fridu <fridulin21@gmail.com>
2023-03-09 16:14:23 +0100

    Aufgaben weiter geführt

--- src/_11_String_Methoden/_11_String_Methoden_Vertiefung/MainApp_A2.java
+++ src/_11_String_Methoden/_11_String_Methoden_Vertiefung/MainApp_A2.java
@@ -1,12 +1,13 @@
 package _11_String_Methoden._11_String_Methoden_Vertiefung;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MainApp_A2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("----------------------");
-        System.out.println("Welche Methode willst du verwenden? \nConcat-Operator[1] \nCharAt[2] \ncompareTo[3] \ncontains[4] \nendsWith[5] \nequals[6] \nindexOf[7] \nindexOf2.0[8] \nisEmpty[9] \njoin[10] \nlength[11] \nreplace[12] \nsplit \nstartsWith \nstrip \nsubstring \nsubstring2.0 \ntoLowerCase \ntoUpperCase");
+        System.out.println("Welche Methode willst du verwenden? \nConcat-Operator[1] \nCharAt[2] \ncompareTo[3] \ncontains[4] \nendsWith[5] \nequals[6] \nindexOf[7] \nindexOf2.0[8] \nisEmpty[9] \njoin[10] \nlength[11] \nreplace[12] \nsplit[13] \nstartsWith[14] \nstrip[15] \nsubstring[16] \nsubstring2.0[17] \ntoLowerCase[18] \ntoUpperCase[19]");
         System.out.println("----------------------");
         int input2 = scanner.nextInt();
         switch (input2) {
@@ -26,6 +27,28 @@ public class MainApp_A2 {
                 indexOf();
             case 8:
                 indexOf2();
+            case 9:
+                isEmpty();
+            case 10:
+                join();
+            case 11:
+                lenght();
+            case 12:
+                replace();
+            case 13:
+                split();
+            case 14:
+                startsWith();
+            case 15:
+                strip();
+            case 16:
+                substring();
+            case 17:
+                substring2();
+            case 18:
+                tolowercase();
+            case 19:
+                touppercase();
         }
     }
 
@@ -123,47 +146,117 @@ public class MainApp_A2 {
     }
 
     public static void isEmpty() {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("true, wenn der String eine Länge von 0 hat, sonst false.");
+        System.out.println("Gib einen Satz ein:");
+        String input = scanner.nextLine();
+        boolean empty = input.isEmpty();
+        System.out.println(empty);
     }
 
     public static void join() {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Gibt einen String zurück, in dem alle Elemente des Arrays mit dem Delimiter (= Begrenzer) verbunden wurden.");
+        System.out.println("Gebe einen Satz ein:");
+        String input = scanner.nextLine();
+        String[] wörter = input.split(" ");
+        for (String wort : wörter){
+            System.out.println(wort);
+        }
     }
-
     public static void lenght() {
-
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Gibt die Länge des Strings aus.");
+        System.out.println("Gebe einen Satz ein:");
+        String input = scanner.nextLine();
+        int lenght = input.length();
+        System.out.println(lenght);
     }
 
     public static void replace() {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Gibt einen String zurück, bei dem alle Vorkommen von oldChar durch newChar ersetzt wurden.");
+        System.out.println("Gebe einen Satz ein:");
+        String input = scanner.nextLine();
+        System.out.println("Gebe ein Buchstaben ein der Ersetzt werden soll:");
+        String input2 = scanner.nextLine();
+        System.out.println("Gib einen Buchstaben welcher eingesetzt werden soll:");
+        String input3 = scanner.nextLine();
+        String replace = input.replace(input2,input3);
+        System.out.println(replace);
     }
 
     public static void split() {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Teilt den String überall da, wo der Delimiter gefunden wird, und gibt die einzelnen Strings in einem Array zurück.");
+        System.out.println("Gebe einen Satz ein:");
+        String input = scanner.nextLine();
+        String[] wörter = input.split(" ");
+        for (String wort : wörter) {
+            System.out.println(wort);
+        }
     }
 
     public static void startsWith() {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Gibt den booleschen Wert true zurück, falls der String mit der im Parameter str übergebenen Zeichenkette beginnt. Ansonsten wird false zurückgegeben.");
+        System.out.println("Gebe einen Satz ein:");
+        String input = scanner.nextLine();
+        System.out.println("Gebe ein Buchstaben ein mit welchem dein Satz beginnt:");
+        String input2  =scanner.nextLine();
+        boolean starswith = input.startsWith(input2);
+        System.out.println(starswith);
     }
 
     public static void strip() {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Gibt einen String zurück, bei dem alle Leerzeichen am Anfang und am Ende des Strings entfernt wurden. Leerzeichen in der Mitte bleiben erhalten. Der String selbst wird nicht verändert.");
+        System.out.println("Gebe einen Satz ein:");
+        String input = scanner.nextLine();
+        String strip = input.strip();
+        System.out.println(strip);
     }
 
     public static void substring() {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Gibt einen String zurück, bei dem alle Leerzeichen am Anfang und am Ende des Strings entfernt wurden. Leerzeichen in der Mitte bleiben erhalten. Der String selbst wird nicht verändert.");
+        System.out.println("Gebe einen Satz ein:");
+        String input = scanner.nextLine();
+        System.out.println("Gebe einen Index ein");
+        int indx = scanner.nextInt();
+        String substring = input.substring(indx);
+        System.out.println(substring);
     }
 
     public static void substring2() {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Gibt den Teilstring der bei Position from-idx beginnt und vor Position to-idx endet.");
+        System.out.println("Gebe einen Satz ein:");
+        String input = scanner.nextLine();
+        System.out.println("Gebe Index an:");
+        int idx = scanner.nextInt();
+        System.out.println("Gebe Index an:");
+        int idx2 = scanner.nextInt();
+        String substring = input.substring(idx,idx2);
+        System.out.println(substring);
     }
 
     public static void tolowercase() {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Gibt den String in Kleinbuchstaben zurück – der String selbst wird nicht verändert.");
+        System.out.println("Gebe einen Satz ein:");
+        String input = scanner.nextLine();
+        String lower = input.toLowerCase();
+        System.out.println(lower);
     }
 
     public static void touppercase() {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Gibt den String in Großbuchstaben zurück – der String selbst wird nicht verändert.");
+        System.out.println("Gebe einen Satz ein:");
+        String input = scanner.nextLine();
+        String upper = input.toUpperCase();
+        System.out.println(upper);
     }
 }
 

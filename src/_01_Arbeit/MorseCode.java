1176ce2a329bbf76b8f20c7715f3cc194d07c8d9
fridu <fridulin21@gmail.com>
2023-04-26 18:02:58 +0200

    Aufgaben weiter geführt

---
+++ src/_01_Arbeit/MorseCode.java
@@ -0,0 +1,44 @@
+package _01_Arbeit;
+
+public class MorseCode {
+    public static String[] morsecode() {
+        String[] arr = new String[]{
+                ".-",       //A
+                "-...",     //B
+                "-.-.",     //C
+                "-..",      //D
+                ".",        //E
+                "..-.",     //F
+                "--.",      //G
+                "....",     //H
+                "..",       //I
+                ".---",     //J
+                "-.-",      //K
+                ".-..",     //L
+                "--",       //M
+                "-.",       //N
+                "---",      //O
+                ".--.",     //P
+                "--.-",     //Q
+                ".-.",      //R
+                "...",      //S
+                "-",        //T
+                "..-",      //U
+                "...-",     //V
+                ".--",      //W
+                "-..-",     //X
+                "-.--",     //Y
+                "--.."      //Z
+        };
+        return arr;
+    }
+
+    public static String textToMorse(String[] morsecode, String text) {
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

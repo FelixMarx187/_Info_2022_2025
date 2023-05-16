c094511c979119ae86a91f13fa70a36bda366d7a
felmar211006 <kajdkjad@sdsdsdf>
2023-05-16 13:17:49 +0200

    Aufgaben weiter geführt

---
+++ src/_12_Klassen/_12_Klassen_Einfuehrung/MainApp1.java
@@ -0,0 +1,4 @@
+package _12_Klassen._12_Klassen_Einfuehrung;
+
+public class MainApp1 {
+}
---
+++ src/_12_Klassen/_12_Klassen_Einfuehrung/Rechnen/MainApp.java
@@ -0,0 +1,8 @@
+package _12_Klassen._12_Klassen_Einfuehrung.Rechnen;
+
+public class MainApp {
+    public static void main(String[] args) {
+        Rechnen meinRechner = new Rechnen();
+        System.out.println(meinRechner.istGerade(200));
+    }
+}

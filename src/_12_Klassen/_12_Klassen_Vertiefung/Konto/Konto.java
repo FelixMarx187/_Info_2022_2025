1886a68be27fb08c397e5a65f3b0f1607309a1e2
felmar211006 <kajdkjad@sdsdsdf>
2023-05-23 13:20:53 +0200

    Aufgaben weiter geführt

--- src/_12_Klassen/_12_Klassen_Vertiefung/Konto/Konto.java
+++ src/_12_Klassen/_12_Klassen_Vertiefung/Konto/Konto.java
@@ -3,13 +3,78 @@ package _12_Klassen._12_Klassen_Vertiefung.Konto;
 import java.util.Random;
 
 public class Konto {
-    public String inhaber;
-    public int kontoNummer;
-    public int kontoStand;
-    public int kontoFuehrungsGebueren;
-    public double guthabenZinsen;
-    public int darlehenzinsen;
-    public int maxDarlehensSumme;
+    private String inhaber;
+    private int kontoNummer;
+    private int kontoStand;
+    private int kontoFuehrungsGebueren;
+    private double guthabenZinsen;
+    private int darlehenzinsen;
+    private int maxDarlehensSumme;
+
+    public String getInhaber() {
+        return inhaber;
+    }
+
+    public void setInhaber(String inhaber) {
+        this.inhaber = inhaber;
+    }
+
+    public int getKontoNummer() {
+        return kontoNummer;
+    }
+
+    public void setKontoNummer(int kontoNummer) {
+        this.kontoNummer = kontoNummer;
+    }
+
+    public int getKontoStand() {
+        return kontoStand;
+    }
+
+    public void setKontoStand(int kontoStand) {
+        this.kontoStand = kontoStand;
+    }
+
+    public int getKontoFuehrungsGebueren() {
+        return kontoFuehrungsGebueren;
+    }
+
+    public void setKontoFuehrungsGebueren(int kontoFuehrungsGebueren) {
+        this.kontoFuehrungsGebueren = kontoFuehrungsGebueren;
+    }
+
+    public double getGuthabenZinsen() {
+        return guthabenZinsen;
+    }
+
+    public void setGuthabenZinsen(double guthabenZinsen) {
+        this.guthabenZinsen = guthabenZinsen;
+    }
+
+    public int getDarlehenzinsen() {
+        return darlehenzinsen;
+    }
+
+    public void setDarlehenzinsen(int darlehenzinsen) {
+        this.darlehenzinsen = darlehenzinsen;
+    }
+
+    public int getMaxDarlehensSumme() {
+        return maxDarlehensSumme;
+    }
+
+    public void setMaxDarlehensSumme(int maxDarlehensSumme) {
+        this.maxDarlehensSumme = maxDarlehensSumme;
+    }
+
+    public int getMaxAbhebeBetrag() {
+        return maxAbhebeBetrag;
+    }
+
+    public void setMaxAbhebeBetrag(int maxAbhebeBetrag) {
+        this.maxAbhebeBetrag = maxAbhebeBetrag;
+    }
+
     public int maxAbhebeBetrag;
 
 
--- src/_12_Klassen/_12_Klassen_Vertiefung/Konto/Main.java
+++ src/_12_Klassen/_12_Klassen_Vertiefung/Konto/Main.java
@@ -4,20 +4,29 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Neues Konto erstellen? (Ja[1] / Nein[2])");
-        int input = new Scanner(System.in).nextInt();
+        boolean run = true;
+        while (true) {
+            System.out.println("Haben sie schon ein Konto? (Ja[1] / Nein[2])");
+            int input = new Scanner(System.in).nextInt();
 
-        if (input == 1) {
-            System.out.println("Name: ");
-            String pInhaber = new Scanner(System.in).nextLine();
-            System.out.println("Kontostand: ");
-            int kontostand = new Scanner(System.in).nextInt();
-            Konto konto = new Konto(pInhaber,kontostand);
+            if (input == 2) {
+                System.out.println("Konto Registrierung");
+                System.out.println("Name: ");
+                String pInhaber = new Scanner(System.in).nextLine();
+                System.out.println("Kontostand: ");
+                int kontostand = new Scanner(System.in).nextInt();
+                Konto konto = new Konto(pInhaber,kontostand);
 
-            System.out.println("------------------------------------");
-            System.out.println("Inhaber: " + konto.inhaber);
-            System.out.println("Kontonummer: " + konto.kontoNummer);
-            System.out.println("Kontostand: " + konto.kontoStand + " €");
+                System.out.println("------------------------------------");
+                System.out.println("Inhaber: " + konto.getInhaber());
+                System.out.println("Kontonummer: " + konto.getKontoNummer());
+                System.out.println("Kontostand: " + konto.getKontoStand() + " €");
+                int counter = 0;
+                Konto[][] neuesKonto = new Konto[10][3];
+                String[] konto1 = {konto.getInhaber(), String.valueOf(konto.getKontoNummer()), String.valueOf(konto.getKontoStand())};
+
+
+            }
         }
     }
 }
